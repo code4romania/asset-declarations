038d8c77890115fbc67abb9a6414704df707a947
catileptic <s.alexandra.stefanescu@gmail.com>
2019-01-21 22:16:56 +0200

    Revert "Add new UI and General Information task (incomplete)."

--- project_template/forms.py
+++ project_template/forms.py
@@ -1,15 +1,6 @@
 from django import forms
 
 
-class TranscribeInitialInformation(forms.Form):
-    name = forms.CharField(label="What is the name of the current politician?")
-    surname = forms.CharField(label="What is the surname of the current politician?")
-
-
 class SimpleForm(forms.Form):
     title = forms.CharField(label="What is the title of the document?")
     non_readable = forms.BooleanField(label="I can't read it", required=False)
-
-
-class TranscribeNumberOfRowsPerTableForm(forms.Form):
-    number_of_rows = forms.IntegerField(label="Cate randuri are acest tabel?")
