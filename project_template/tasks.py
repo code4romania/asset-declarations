5e39f37de8e4a98d1eef8329a23a465a097dac72
paubric <paubric@gmail.com>
2019-01-26 16:49:37 +0200

    Fix naming consistency in tasks.py

--- project_template/tasks.py
+++ project_template/tasks.py
@@ -216,7 +216,7 @@ class TaskTranscribeIndependentActivities(AbstractTask):
         return super(TaskTranscribeIndependentActivities, self).get_presenter()
 
     def save_verified_data(self, verified_data):
-        count, created = models.OwnedIncomeFromIndependentActivities.objects.get_or_create(
+        owned_income_from_independent_activities, created = models.OwnedIncomeFromIndependentActivities.objects.get_or_create(
             count=verified_data['count'],
         )
 
@@ -343,6 +343,32 @@ class TaskOwnedIncomeFromPensionsTable(AbstractTask):
 
     def save_verified_data(self, verified_data):
         owned_income_from_pensions_table, created = models.OwnedIncomeFromPensionsTable.objects.get_or_create(
+            count=verified_data['count'],
+        )
+
+    def after_save(self, verified_data):
+        # Create a new task for each table, asking the user to transcribe the number of rows
+        pass
+
+
+@register()
+class TaskOwnedGoodsOrServicesPerChildTable(AbstractTask):
+    task_form = forms.TranscribeOwnedGoodsOrServicesPerChildTable
+    template_name = 'tasks/row_count_template.html'
+
+    def create_mocked_task(self, task_data):
+        task_data['info'].update({
+            'url': 'http://www.cdep.ro/declaratii/deputati/2016/avere/002a.pdf',
+            'page': 10
+        })
+
+        return task_data
+
+    def get_presenter(self):
+        return super(TaskOwnedIncomeFromPensionsTable, self).get_presenter()
+
+    def save_verified_data(self, verified_data):
+        owned_income_from_pensions_table, created = models.OwnedGoodsOrServicesPerChildTable.objects.get_or_create(
             count=verified_data['count'],
         )
 
